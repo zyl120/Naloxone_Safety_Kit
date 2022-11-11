108272be97392618f6c9585fa3ff0c822a0af543
Yilu_RPi <yz2797@cornell.edu>
2022-11-11 00:01:03 -0500

    add monitoring process

--- main/main.py
+++ main/main.py
@@ -1,8 +1,13 @@
 # This is the main function for the design project Internet-based Naloxone
-# safety kit. 
+# safety kit.
 
 import os
+from time import sleep
+import psutil
 from temperature_fan import temperature_fan
+
+temperature_fan_pid = 0
+
 
 def fork_temperature():
     pid = os.fork()
@@ -12,7 +17,17 @@ def fork_temperature():
     else:
         # this is the child process
         print("child pid=" + str(pid))
+        temperature_fan()
+    return pid
 
 
 if __name__ == "__main__":
-    fork_temperature()
+    temperature_fan_pid = fork_temperature()
+    while True:
+        _, temperature_fan_exit_status = os.waitpid(
+            temperature_fan_pid, os.WNOHANG)
+        print(temperature_fan_exit_status)
+        if (temperature_fan_exit_status != 0):
+            print("fork...")
+            temperature_fan_pid = fork_temperature()
+        sleep(3)
--- main/temperature_fan.py
+++ main/temperature_fan.py
@@ -7,22 +7,27 @@ DHT_PIN = 27
 def read_temperature_sensor():
     humidity, temperature = dht.read_retry(dht.DHT22, DHT_PIN)
     # Print Temperature and Humidity on Shell window
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+    #print(str(temperature) + ", " + str(humidity))
     #sleep(5)  # Wait 5 seconds and read again
     return temperature
 
 def calculate_pwm(temperature):
     print("control pwm")
+    pwm = 0
+    return pwm
 
 
 def control_fan(pwm):
-    print("controling fan pwm")
+    return True
+    #print("controling fan pwm")
 
 
 def temperature_fan():
-    temperature = read_temperature_sensor()
-    pwm = calculate_pwm(temperature)
-    control_fan(pwm)
+    while True:
+        temperature = read_temperature_sensor()
+        pwm = calculate_pwm(temperature)
+        control_fan(pwm)
+        sleep(5)
 
 if __name__ == '__main__':
     print("testing temperature and fan functions")
