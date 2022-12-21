97794607708ab4535b2a6edfbd1d41f829439f39
Yilu Zhou <zhouyilu0120@outlook.com>
2022-12-20 19:29:53 -0500

    change text size

--- main/ui_door_close_window.py
+++ main/ui_door_close_window.py
@@ -2051,8 +2051,8 @@ class Ui_door_close_main_window(object):
         self.closeSoftwareLabel.setBuddy(self.exitPushButton)
 
         self.retranslateUi(door_close_main_window)
-        self.stackedWidget.setCurrentIndex(1)
-        self.settingsTab.setCurrentIndex(2)
+        self.stackedWidget.setCurrentIndex(2)
+        self.settingsTab.setCurrentIndex(3)
         self.passcodeEnterLineEdit.returnPressed.connect(self.passcodeEnterPushButton.click) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(door_close_main_window)
         door_close_main_window.setTabOrder(self.settingsTab, self.passcodeEnterLineEdit)
