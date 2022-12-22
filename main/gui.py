50c0d6aade6f9cf88368f930d3f3a57ae672b83e
Yilu Zhou <zhouyilu0120@outlook.com>
2022-12-21 23:22:50 -0500

    adjust scrollable area

--- main/gui.py
+++ main/gui.py
@@ -10,9 +10,9 @@ from ui_door_close_window import Ui_door_close_main_window
 from time import sleep
 import qrcode
 import random
-from gpiozero import CPUTemperature
-import RPi.GPIO as GPIO
-import Adafruit_DHT as dht
+#from gpiozero import CPUTemperature
+#import RPi.GPIO as GPIO
+#import Adafruit_DHT as dht
 
 
 DOOR_PIN = 17
@@ -86,8 +86,8 @@ class IOWorker(QtCore.QThread):
 
     def __init__(self, disarmed, max_temp, expiration_date):
         super(IOWorker, self).__init__()
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(DOOR_PIN, GPIO.IN)
+        #GPIO.setmode(GPIO.BCM)
+        #GPIO.setup(DOOR_PIN, GPIO.IN)
         print("gpio thread go " + str(disarmed) + " " + str(max_temp))
         self.naloxone_counter = 9
         self.naloxone_temp = 25
@@ -99,10 +99,9 @@ class IOWorker(QtCore.QThread):
         self.expiration_date = expiration_date
 
     def read_naloxone_sensor(self):
-        _, self.temperature = dht.read_retry(dht.DHT22, DHT_PIN)
-        #self.naloxone_temp = int(25*1.8+32)
-        # return int(25 * 1.8 + 32)
-
+        #_, self.temperature = dht.read_retry(dht.DHT22, DHT_PIN)
+        self.temperature = 25
+        
     def calculate_pwm(self):
         #print("control pwm")
         list1 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
@@ -113,17 +112,15 @@ class IOWorker(QtCore.QThread):
         return
 
     def read_cpu_sensor(self):
-        self.cpu_temp = int(CPUTemperature().temperature * 1.8 + 32)
-        # print(cpu.temperature)
-        #self.cpu_temp = int(50*1.8+32)
-        # return int(50 * 1.8 + 32)
+        #self.cpu_temp = int(CPUTemperature().temperature * 1.8 + 32)
+        self.cpu_temp = 100
 
     def read_door_sensor(self):
-        #self.door_opened = False
-        if GPIO.input(DOOR_PIN):
-            self.door_opened = True
-        else:
-            self.door_opened = False
+        self.door_opened = False
+        # if GPIO.input(DOOR_PIN):
+        #     self.door_opened = True
+        # else:
+        #     self.door_opened = False
 
     def is_expiry(self):
         today = QtCore.QDate().currentDate()
