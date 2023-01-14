e536abbb6f817da0a9c02df04e8d1c7e4f179f95
Yilu Zhou <zhouyilu0120@outlook.com>
2023-01-13 20:19:01 -0500

    first draft of install script

--- scripts/install.py
+++ scripts/install.py
@@ -1,8 +1,5 @@
 import subprocess, os, sys, apt
 
-def error_print(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
 if __name__ == "__main__":
     if os.geteuid() != 0:
         sys.exit("[E] You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
@@ -12,78 +9,109 @@ if __name__ == "__main__":
         if(output != "GNU/Linux"):
             sys.exit("[E] Wrong OS Type, exit.")
 
+    username = input("Enter your user name: ")
+    print("[I] Current working directory is {}".format(os.getcwd()))
+    try:
+        home_dir = os.path.expanduser("~"+username)
+        print("[I] It will now be changed to your home directory {}".format(home_dir))
+        os.chdir(os.path.expanduser("~"+username))
+    except Exception as e:
+        sys.exit("[E] Invalid user name.")
+    else:
+        print("[I] Current working directory is {}".format(os.getcwd()))
+
     print("[I] Refresh repo list...")
     cache = apt.cache.Cache()
     cache.update()
     cache.open()
 
-    print("[I] Installing python3-pyqt5...")
-    pkg_name = "python3-pyqt5"
-    pkg = cache[pkg_name]
-    if pkg.is_installed:
-        print("[I] {pkg_name} already installed".format(pkg_name=pkg_name))
-    else:
-        pkg.mark_install()
+    pkg_name_list = ["python3-pyqt5", "mpg123", "git", "build-essential","qtdeclarative5-dev", "libqt5svg5-dev", "qtbase5-private-dev", "qml-module-qtquick-controls2", "qml-module-qtquick-controls", "qml-module-qt-labs-folderlistmodel", "libxcb-composite0-dev", "libxcb-cursor-dev", "libxcb-damage0-dev", "libxcb-dpms0-dev", "libxcb-dri2-0-dev", "libxcb-dri3-dev", "libxcb-ewmh-dev", "libxcb-glx0-dev", "libxcb-icccm4-dev", "libxcb-image0-dev", "libxcb-imdkit-dev", "libxcb-keysyms1-dev", "libxcb-present-dev", "libxcb-randr0-dev", "libxcb-record0-dev", "libxcb-render-util0-dev", "libxcb-render0-dev", "libxcb-res0-dev", "libxcb-screensaver0-dev", "libxcb-shape0-dev", "libxcb-shm0-dev", "libxcb-sync-dev", "libxcb-util-dev", "libxcb-util0-dev", "libxcb-xf86dri0-dev", "libxcb-xfixes0-dev", "libxcb-xinerama0-dev", "libxcb-xinput-dev", "libxcb-xkb-dev", "libxcb-xrm-dev", "libxcb-xtest0-dev", "libxcb-xv0-dev", "libxcb-xvmc0-dev", "libxcb1-dev", "libx11-xcb-dev", "libglu1-mesa-dev", "libxrender-dev", "libxi-dev", "libxkbcommon-dev", "libxkbcommon-x11-dev"]
+    for pkg_name in pkg_name_list:
+        pkg = cache[pkg_name]
+        print("[I] Installing {pkg_name}...".format(pkg_name=pkg_name))
+        if pkg.is_installed:
+            print("[I] {pkg_name} already installed".format(pkg_name=pkg_name))
+        else:
+            pkg.mark_install()
+            try:
+                cache.commit()
+            except Exception as e:
+                sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
+            else:
+                print("[i] {pkg_name} installed successfully".format(pkg_name=pkg_name))
 
+    pip_list = ["twilio", "qrcode", "Adafruit-DHT", "gtts", "phonenumbers"]
+    for pip_pkg in pip_list:
+        print("[I] Installing {} using pip...".format(pip_pkg))
         try:
-            cache.commit()
+            subprocess.check_call([sys.executable, "-m", "pip", "install", pip_pkg], stdout=subprocess.DEVNULL)
         except Exception as e:
-            sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
+            sys.exit("[E] Sorry, package installation of {} failed [{}]".format(pip_pkg, str(e)))
+        else:
+            print("[I] {} installed successfully".format(pip_pkg))
     
-    print("[I] Installing mpg123...")
-    pkg_name = "mpg123"
-    pkg = cache[pkg_name]
-    if pkg.is_installed:
-        print("[I] {pkg_name} already installed".format(pkg_name=pkg_name))
+
+    print("[I] Probing QT_PREFIX_PATH...")
+    from PyQt5.QtCore import QLibraryInfo
+    QT_PREFIX_PATH = QLibraryInfo.location(QLibraryInfo.PrefixPath)
+    print("[I] QT_PREFIX_PATH={}".format(QT_PREFIX_PATH))
+
+    sys.exit(0)
+
+    print("[I] Downloading Qt Virtual Keyboard from GitHub...")
+    try:
+        subprocess.check_call(["git", "clone", "-b", "5.15", "https://github.com/qt/qtvirtualkeyboard.git"], stdout = subprocess.DEVNULL)
+    except Exception as e:
+        sys.exit("Failed to download Qt Virtual Keyboard [{}]".format(str(e)))
     else:
-        pkg.mark_install()
+        print("[I] Download Successfully")
 
-        try:
-            cache.commit()
-        except Exception as e:
-            sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
+    new_path = os.getcwd() + "/qtvirtualkeyboard"
+    print("[I] Change directory to {}".format{new_path})
+    os.chdir(new_path)
     
-    print("[I] Installing twilio using pip...")
+    print("[I] Running qmake...")
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "twilio"], stdout=subprocess.DEVNULL)
+        subprocess.check_call(["qmake"], stdout = subprocess.DEVNULL)
     except Exception as e:
-        sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
-    finally:
-        print("[I] twilio installed successfully")
+        sys.exit("Failed to run qmake [{}]".format(str(e)))
+    else:
+        print("[I] qmake Successfully")
 
-    print("[I] Installing qrcode using pip...")
+    print("[I] Running make...")
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "qrcode"], stdout=subprocess.DEVNULL)
+        subprocess.check_call(["make"], stdout = subprocess.DEVNULL)
     except Exception as e:
-        sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
-    finally:
-        print("[I] qrcode installed successfully")
+        sys.exit("Failed to run make [{}]".format(str(e)))
+    else:
+        print("[I] make Successfully")
 
-    print("[I] Installing Adafruit-DHT using pip...")
+    print("[I] Running make install...")
     try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "Adafruit-DHT"], stdout=subprocess.DEVNULL)
+        subprocess.check_call(["make", "install"], stdout = subprocess.DEVNULL)
     except Exception as e:
-        sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
-    finally:
-        print("[I] Adafruit-DHT installed successfully")
-
-    print("[I] Installing gtts using pip...")
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "gtts"], stdout=subprocess.DEVNULL)
-    except Exception as e:
-        sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
-    finally:
-        print("[I] gtts installed successfully")
-
-    print("[I] Installing phonenumbers using pip...")
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "phonenumbers"], stdout=subprocess.DEVNULL)
-    except Exception as e:
-        sys.exit("[E] Sorry, package installation failed [{}]".format(str(e)))
-    finally:
-        print("[I] phonenumbers installed successfully")
-
-    print("[I] Basic Package Set Installed Successfully")
+        sys.exit("Failed to run make install [{}]".format(str(e)))
+    else:
+        print("[I] make install Successfully")
     
+    print("[I] Moving Files...")
+    try:
+        subprocess.check_call(["cp", "{}/lib/libQt5VirtualKeyboard.so.5.15.8".format(new_path), "{}/lib/libQt5VirtualKeyboard.so.5".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["mkdir", "{}/plugins".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["mkdir", "{}/plugins/platforminputcontexts".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["cp", "{}/plugins/platforminputcontexts/libqtvirtualkeyboardplugin.so".format(new_path), "{}/plugins/platforminputcontexts/".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["cp", "-r", "{}/plugins/virtualkeyboard/".format(new_path), "{}/plugins/".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["mkdir", "{}/qml".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["mkdir", "{}/qml/QtQuick".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+        subprocess.check_call(["cp", "-r", "{}/qml/QtQuick/VirtualKeyboard/".format(new_path), "{}/qml/QtQuick/".format(QT_PREFIX_PATH)], stdout = subprocess.DEVNULL)
+    except Exception as e:
+        sys.exit("[E] Failed to move files. [{}]".format(str(e)))
+    else:
+        print("[I] Moving Files Successfully")
 
+    print("[I] Install Successfully")
+    sys.exit(0)
+
+
+    
     
